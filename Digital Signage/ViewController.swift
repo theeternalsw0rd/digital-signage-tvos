f62958757dbe5949f26f094d6fa1d5e0bbf9c86d
Micah Bucy <micah.bucy@eternalsword.com>
2015-12-17 11:15:11 -0500

    Initial Commit

---
+++ Digital Signage/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Digital Signage
+//
+//  Created by Micah Bucy on 12/17/15.
+//  Copyright © 2015 Micah Bucy. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
